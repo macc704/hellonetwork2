bd48a3257c9eb8db536cd232e6903bba69c306ee
Yoshiaki Matsuzawa <yoshiaki.matsuzzawa@gmail.com>
2013-09-10 16:24:19 +0900

    inputできるようにしたクライアント

--- hellonetwork3/src/hellonetwork3/ClientMain.java
+++ hellonetwork3/src/hellonetwork3/ClientMain.java
@@ -1,5 +1,7 @@
 package hellonetwork3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -9,16 +11,27 @@ public class ClientMain {
 	public static void main(String[] args) throws Exception {
 		ClientMain main = new ClientMain();
 		main.run();
-
+//main.test();
+	}
+	
+	void test() throws Exception {
+		System.out.println(input());
 	}
 
 	void run() throws Exception {
 		Socket sock = new Socket("192.168.1.163", 10000);		
 		OutputStream out = sock.getOutputStream();
 		ObjectOutputStream ois = new ObjectOutputStream(out);
-		ois.writeObject("Test");
+		System.out.print(">>");
+		String input = input();
+		ois.writeObject(input);
 		out.close();
 		sock.close();
+	}
+	
+	String input() throws Exception{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		return reader.readLine();
 	}
 
 }
